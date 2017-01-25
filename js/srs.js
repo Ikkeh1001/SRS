0698caf584c2667410f775ff71d72038f45ea632
Ikkeh1001 <thijs.moen@gmail.com>
2017-01-25 20:54:17 +0100

    Embarrassing bug fix

--- js/srs.js
+++ js/srs.js
@@ -60,7 +60,6 @@ $.post("api.php", {
   $("#listtablepage").html(differenthtml);
   response.forEach(function(element){
     var thisstatus = element["status"];
-    if (thisstatus.substring(3,4) == "1" && parseInt(element["timestamp"])+xxx1 < $.now()){
       if(wordnumber>=14*currentpage || wordnumber<(currentpage-1)*14){
         this.wordnumber++;
         return;
@@ -69,19 +68,7 @@ $.post("api.php", {
       var differenthtml = "<tr class='removeable listcontent' style='width:100%; height: 5%;'><td style='width:33%;'>"+element["word"]+"</td><td style='width:33%;'>"+element["meaning"]+"</td><td style='width:33%;'>Level "+element["status"].substring(0,1)+"</td></tr>";
       $("#listtable").append(differenthtml);
       return;
-    }
-    var vartime = thisstatus.substring(0,2);
-    vartime = "t"+vartime+"xx";
-    if (parseInt(element["timestamp"])+ window[vartime] < $.now()){
-      if(wordnumber>=14*currentpage || wordnumber<(currentpage-1)*14){
-        this.wordnumber++;
-        return;
-      }
-      this.wordnumber++;
-      var differenthtml = "<tr class='removeable listcontent' style='width:100%; height: 5%;'><td style='width:33%;'>"+element["word"]+"</td><td style='width:33%;'>"+element["meaning"]+"</td><td style='width:33%;'>Level "+element["status"].substring(0,1)+"</td></tr>";
-      $("#listtable").append(differenthtml);
-      return;
-    }
+
   });
   var differenthtml = "<tr class='removeable' style='width:100%; height: "+(70-5*(wordnumber-((currentpage-1)*14)))+"'></tr>";
   $("#listtable").append(differenthtml);
